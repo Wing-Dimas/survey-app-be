216b3d5236913714efdf330f77e25fe4e761b530
Wing-Dimas <wingdimas@gmail.com>
2024-12-19 09:30:50 +0700

    fix(api): add credential fields to response api

--- app/Http/Controllers/Api/V1/FormSubmissionController.php
+++ app/Http/Controllers/Api/V1/FormSubmissionController.php
@@ -17,11 +17,32 @@ class FormSubmissionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         try {
             $formSubmissions = FormSubmission::with('options')->get();
-            return ResponseFormatter::success(new FormSubmissionCollection($formSubmissions),'Get form submissions successfully');
+            $user = [
+                [
+                    'id' => null,
+                    'field_name' => 'email',
+                    'type' => 'email',
+                    'required' => true,
+                    'options' => null
+                ],
+                [
+                    'id' => null,
+                    'field_name' => 'name',
+                    'type' => 'text',
+                    'required' => true,
+                    'options' => null,
+                ]
+            ];
+
+            Log::warning("user get form submissions successfully", ['app_name' => $request->app_name, 'api_key' => $request->api_key_id]);
+            return ResponseFormatter::success([
+                'credentials' => $user,
+                'form_submissions' => new FormSubmissionCollection($formSubmissions)],'Get form submissions successfully'
+            );
         } catch (\Throwable $th) {
             Log::error(flattenError($th));
             return ResponseFormatter::error(null,'Internal server error', 500)->setStatusCode(500);
